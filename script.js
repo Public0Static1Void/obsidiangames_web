2b9064aaf2966894661d92d124e500720b412135
Dani González Peralta <d71024765@gmail.com>
2026-04-14 00:07:30 +0200

    Improved web and logos

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const hero = document.getElementById("hero");
+const up_logo = document.getElementById("up_logo");
 
-let ticking = false;
+let ticking = false, scrolled = false;
+
+const swap_logo = (newSource) => {
+    up_logo.style.opacity = 0;
+
+    setTimeout(() => {
+        up_logo.src = newSource;
+        up_logo.style.opacity = 1;
+    }, 250);
+}
 
 const handleScroll = () => {
     // Hysteresis: enter at 120, exit only at 70
-    if (window.scrollY >= 300) {
+    if (window.scrollY >= 10 && !scrolled) {
         hero.classList.add("scrolled");
-    } else if (window.scrollY <= 119) {
-        hero.classList.remove("scrolled");
+        swap_logo("images/Obsidian logos/obsidian_logo_red_text_small.png");
+
+        scrolled = true; // Don't repeat the scroll actions
     }
     ticking = false;
 };
